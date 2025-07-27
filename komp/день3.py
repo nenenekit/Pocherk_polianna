88fd4e10a0e3a7ad41d5ffff9a3504acc786ca31
N_OFFICE_4 <example@mail.com>
2025-07-27 22:43:17 +0300

    fufu

---
+++ komp/день3.py
@@ -0,0 +1,12 @@
+for i in range(5):
+    print(i)
+счетчик=5
+while счетчик>0:
+    print(счетчик)
+    счетчик-=1
+игры=['roblox']
+игры.append('cfgdhfj')
+игры.remove('roblox')
+print(игры[0])
+for игра in игры:
+    print(игра)
